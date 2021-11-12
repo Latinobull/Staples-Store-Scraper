6dc8e326c5c99fd22d0426fdd1e9a32cbca041da
Donnahue George <Donnahuegjr@gmail.com>
2021-11-12 09:43:51 -0500

    headledss browser make code run faster

--- back.py
+++ back.py
@@ -8,25 +8,26 @@ def Start(storeNumber):
     PATH = "C:\Program Files (x86)\chromedriver.exe"
     PAGE = f"https://stores.staples.com/search?storeId={storeNumber}&qp={storeNumber}&l=en"
 
-    driver = webdriver.Chrome(PATH)
+    options = webdriver.ChromeOptions()
+    options.add_argument('--incognito')
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(PATH, chrome_options=options)
     driver.get(PAGE)
 
-    while True:
-        text = driver.find_element(By.CLASS_NAME, 'Teaser-titleLink').text
-        print(text)
-        driver.find_element(By.CLASS_NAME, 'Teaser-titleLink').click()
-        time.sleep(1)
-        storeNum = driver.find_element(By.CLASS_NAME, 'Core-storeId').text
-        street = driver.find_element(By.CLASS_NAME, 'c-address-street-1').text
-        city = driver.find_element(By.CLASS_NAME, 'c-address-city').text
-        state = driver.find_element(By.CLASS_NAME, 'c-address-state').text
-        zipcode = driver.find_element(
-            By.CLASS_NAME, 'c-address-postal-code').text
-        phone = str(driver.find_element(By.ID, 'phone-main').text)
-        print(
-            f'{storeNum} is located at {street} {city}, {state} {zipcode}. The phone number is {phone}')
-        driver.quit()
-        break
+    text = driver.find_element(By.CLASS_NAME, 'Teaser-titleLink').text
+    print(text)
+    driver.find_element(By.CLASS_NAME, 'Teaser-titleLink').click()
+    time.sleep(1)
+    storeNum = driver.find_element(By.CLASS_NAME, 'Core-storeId').text
+    street = driver.find_element(By.CLASS_NAME, 'c-address-street-1').text
+    city = driver.find_element(By.CLASS_NAME, 'c-address-city').text
+    state = driver.find_element(By.CLASS_NAME, 'c-address-state').text
+    zipcode = driver.find_element(
+        By.CLASS_NAME, 'c-address-postal-code').text
+    phone = str(driver.find_element(By.ID, 'phone-main').text)
+    print(
+        f'{storeNum} is located at {street} {city}, {state} {zipcode}. The phone number is {phone}')
+    driver.quit()
 
 
 Start(1232)

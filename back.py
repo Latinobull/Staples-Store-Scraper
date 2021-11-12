e474485242a3f4384e9e59019e7aa22333e1c6cd
Donnahue George <Donnahuegjr@gmail.com>
2021-11-12 11:29:12 -0500

    remove redundant code to make code move faster

--- back.py
+++ back.py
@@ -18,16 +18,6 @@ def Start(storeNumber):
     text = driver.find_element(By.CLASS_NAME, 'Teaser-titleLink').text
     print(text)
     driver.find_element(By.CLASS_NAME, 'Teaser-titleLink').click()
-    time.sleep(1)
-    storeNum = driver.find_element(By.CLASS_NAME, 'Core-storeId').text
-    street = driver.find_element(By.CLASS_NAME, 'c-address-street-1').text
-    city = driver.find_element(By.CLASS_NAME, 'c-address-city').text
-    state = driver.find_element(By.CLASS_NAME, 'c-address-state').text
-    zipcode = driver.find_element(
-        By.CLASS_NAME, 'c-address-postal-code').text
-    phone = str(driver.find_element(By.ID, 'phone-main').text)
-    print(
-        f'{storeNum} is located at {street} {city}, {state} {zipcode}. The phone number is {phone}')
     source = driver.page_source
     driver.quit()
     Soupify(source)
